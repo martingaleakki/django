a56bce77133b1d23f45f8d9f9a784b1be334a72b
Akhil Lodha <akhillodha@akhils-mbp-2.gateway.pace.com>
2014-02-17 17:50:41 -0800

    Updated some pages

--- slicedinvesting/slicedinvesting/settings.py
+++ slicedinvesting/slicedinvesting/settings.py
@@ -31,6 +31,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+ACCOUNT_OPEN_SIGNUP = False
 
 # Application definition
 
@@ -163,3 +164,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
--- slicedinvesting/slicedinvesting/urls.py
+++ slicedinvesting/slicedinvesting/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('landing.urls')),
-     url(r'^landing/', include('landing.urls')),
+    url(r'^landing/', include('landing.urls')),
 )
