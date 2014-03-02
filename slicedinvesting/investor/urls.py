a9f495539d345d5688a1e00eb6732aa70e9a678f
martingaleakki <lodhaakhil85@gmail.com>
2014-03-01 21:47:34 -0800

    login now works

--- slicedinvesting/investor/urls.py
+++ slicedinvesting/investor/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import patterns, url
 from investor import views
 
 urlpatterns = patterns('',
-        url(r'^register/', views.createInvestor, name='index'))
+        url(r'^login/', views.loginInvestor, name='login'))
--- slicedinvesting/investor/views.py
+++ slicedinvesting/investor/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
-def createInvestor(request):
+def loginInvestor(request):
     #if request.method == 'POST':
     
-    context = RequestContext(request)
-
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {}
     
+    username = request.POST['username']
+    password = request.POST['password']
+    print "username" + username
+    print "password" + password
+    user = authenticate(username=username, password=password)
+    print user
     data = request.POST
-    print data['username']
-    print data['email']
+    print data
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            print "Authenticated User" +username
+            return HttpResponseRedirect('/profiles/'+username)
+        else:
+            return HttpResponseRedirect('/landing/')
+    #print data['email']
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('slicedinvesting/landing/base.html', context_dict, context)
+    return HttpResponseRedirect('/landing/')
+    #return render_to_response('slicedinvesting/landing/base.html', context_dict, context)
+
+
+
+    
